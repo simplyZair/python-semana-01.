59b83667f8fb31c9f791c9f54c84b58198fb8f10
Fernando Ortiz <fernandozair@hotmail.com>
2026-08-21 08:38:53 -0600

    Lee las tres calificaciones decimales, calcula el promedio y muestralo con dos cifras decimales

---
+++ promedio.py
@@ -0,0 +1,8 @@
+calificacion_1 = float(input("Introduzca la calificación 1: "))
+calificacion_2 = float(input("Introduzca la calificación 2: "))
+calificacion_3 = float(input("Introduzca la calificación 3: "))
+promedio = (calificacion_1 + calificacion_2 + calificacion_3) / 3
+
+print(f"El promedio es: {promedio:.2f}")
+
+#Fernando Ortiz

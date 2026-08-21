5c669fe1a74aa91007934e182b83de3c8dd0249a
Fernando Ortiz <fernandozair@hotmail.com>
2026-08-21 08:39:34 -0600

    Solicita nombre, edad y carrera. Muestra una oracion con los tres datos mediante una cadena f

--- datos_personales.py
+++ datos_personales.py
@@ -5,3 +5,11 @@ carrera = str(input("Ingrese su carrera: "))
 print("Su nombre es ", nombre)
 print("Su edad es ", edad)
 print("Su carrera es ", carrera)
+
+#
+#Fernando Ortiz
+#nombre = input("Introduzca su nombre: ")
+#edad = int(input("Introduzca su edad: "))
+#carrera = input("Introduzca el nombre de su carrera: ")
+
+#print(f"{nombre}, {edad}, {carrera}")
---
+++ intro.py
@@ -0,0 +1,17 @@
+import keyword
+#Lista las palabras reservadas de Python
+print("LISTA DE PALABRAS RESERVADAS DE PYTHON")
+print(keyword.kwlist)
+
+#Verifica si un token es una palabra reservada o no.
+print("¿while es una palabra reservada? Respuesta = "+ str(keyword.iskeyword 
+                                                           ("while")))
+print("¿estudiante es una palabra reservada? Respuesta = "+ str(keyword.iskeyword
+                                                                ("estudiante")))
+
+x = 8
+y = 3
+z = x/y
+
+print(f"{z:.2f}")
+print(f"{x}")

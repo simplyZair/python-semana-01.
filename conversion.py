953cf5777a4206e8ce4410fa952ad9f5d826ef40
Fernando Ortiz <fernandozair@hotmail.com>
2026-08-21 08:38:23 -0600

    Lee una cantidad de horas y calcula cuantos minutos y segundos representa

---
+++ conversion.py
@@ -0,0 +1,6 @@
+horas = float(input("Introduzca la cantidad de horas: "))
+
+minutos = horas * 60
+segundos = horas * 3600
+
+print(f"{horas} horas equivalen a {minutos} minutos y {segundos} segundos.")
